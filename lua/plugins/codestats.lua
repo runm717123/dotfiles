f8e36ad4702b6b20b01f44ca4910855379476d62
nurm717123 <nur@softwareseni.com>
2024-11-18 19:44:45 +0700

    setup codestat

--- lua/plugins/codestats.lua
+++ lua/plugins/codestats.lua
@@ -1,16 +1,19 @@
--- Load the configuration from the external file
-local api_keys = require("api_keys")
-
 return {
   {
-    "code-stats-vim",
-    -- repo = "https://gitlab.com/code-stats/code-stats-vim",
-    repo = "gitlab:code-stats/code-stats-vim",
+    "liljaylj/codestats.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "TextChanged", "InsertEnter" },
     config = function()
-      -- Use the API key, custom URL, and username from the external file
-      vim.g.codestats_api_key = api_keys.codestats.api_keys
-      vim.g.codestats_url = api_keys.codestats.url
-      vim.g.codestats_username = api_keys.codestats.username
+      local api_key = os.getenv("CODESTATS_API_KEY")
+      require("codestats").setup({
+        username = "nur", -- Replace with your Code::Stats username
+        api_key = api_key, -- Replace with your Code::Stats API key
+        base_url = "https://exp.seni.cloud", -- Default Code::Stats base URL
+        send_on_exit = true, -- Send XP when Neovim exits
+        send_on_timer = true, -- Send XP on a timer
+        timer_interval = 60000, -- Timer interval in milliseconds (default: 60s)
+        curl_timeout = 5, -- Timeout for curl requests in seconds
+      })
     end,
   },
 }
