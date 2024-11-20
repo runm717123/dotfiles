627730085a04f20c5bcae1d9926e2f6681bb8e03
nurm717123 <nur@softwareseni.com>
2024-11-20 16:00:45 +0700

    zen mode

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 vim.opt.cmdheight = 2 -- Increase command-line height
 vim.g.python3_host_prog = "C:\\Windows\\py.exe"
 
+vim.o.fileformats = "dos"
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
