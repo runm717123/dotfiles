008867c8939fcf6529f8348632b2dd51cbf3c288
nur <runm717123@gmail.com>
2024-11-24 22:24:06 +0700

    winter is coming, unix auto format

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@ vim.opt.cmdheight = 2 -- Increase command-line height
 vim.g.python3_host_prog = "C:\\Windows\\py.exe"
 
 vim.o.fileformats = "dos"
+vim.opt.fileformats = { "dos", "unix" } -- Accept both CRLF and LF
+
+-- avoid eslint warning to insert "cr"
+vim.cmd([[
+  autocmd BufWritePre * :set fileformat=unix
+]])
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
