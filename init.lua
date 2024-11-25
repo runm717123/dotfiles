2580478ebc82045196dbd902d5db07e968a4e668
nurm717123 <nur@softwareseni.com>
2024-11-25 18:07:19 +0700

    init auto import

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@ vim.opt.cmdheight = 2 -- Increase command-line height
 vim.g.python3_host_prog = "C:\\Windows\\py.exe"
 
 vim.opt.fileformats = "dos"
--- vim.opt.fileformats = { "dos", "unix" } -- Accept both CRLF and LF
+vim.opt.fileformats = { "dos", "unix" } -- Accept both CRLF and LF
 
 -- avoid eslint warning to insert "cr"
--- vim.cmd([[
---   autocmd BufWritePre * :set fileformat=unix
--- ]])
+vim.cmd([[
+  autocmd BufWritePre * :set fileformat=unix
+]])
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>bh", ":BufferLineMovePrev<CR>", { desc = "Move Buffer Backward" })
+vim.keymap.set("n", "<leader>bl", ":BufferLineMoveNext<CR>", { desc = "Move Buffer Forward" })
