554174b04e3e0e13668b3eccfb98b317844397e3
nur <runm717123@gmail.com>
2024-11-27 20:18:01 +0700

    setup cowboy and keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+local cowboy = require("mine.cowboy")
+
+cowboy()
 
 vim.keymap.set("n", "<leader>bh", ":BufferLineMovePrev<CR>", { desc = "Move Buffer Backward" })
 vim.keymap.set("n", "<leader>bl", ":BufferLineMoveNext<CR>", { desc = "Move Buffer Forward" })
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+local noRemapAndSilent = { noremap = true, silent = true }
+
+-- Ctrl+Backspace: Delete the current line
+vim.keymap.set("i", "<C-h>", "<Esc>cc", opts) -- Insert mode
+-- Alt+Backspace: Delete the previous word
+vim.keymap.set("i", "<A-BS>", "<C-w>", opts) -- Insert mode
