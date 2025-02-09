a794ec8e19c0d54a4d5d973cc7e325a31eb3bc6b
nur <runm717123@gmail.com>
2025-02-09 21:50:33 +0700

    add previe

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,13 @@ return {
         ["s"] = function()
           require("flash").jump() -- Call flash.nvim's jump functionality
         end,
+        ["P"] = {
+          "toggle_preview",
+          config = {
+            use_float = false,
+            title = "Neo-tree Preview",
+          },
+        },
       },
     },
   },
