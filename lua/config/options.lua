fcc4fff97e63525f275f1d50b4482b73a6d82d1c
nur <runm717123@gmail.com>
2025-02-17 20:13:17 +0700

    update and change options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,12 @@ vim.opt.shell = "pwsh"
 vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
 vim.opt.shellquote = ""
 vim.opt.shellxquote = ""
+
+--- Disable hiding of markup
+vim.opt.conceallevel = 0
+
+--- Copy the indent of the current line when inserting a new line
+vim.opt.autoindent = true
+
+--- Disable the inlay hints (additional information about types and parameters names) because it clutters my view
+vim.g.lazyvim_no_inlay_hints = true
