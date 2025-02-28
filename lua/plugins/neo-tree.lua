792d32cdf08d22bf1513bac843f200bce1c1bd69
nurm717123 <nur@softwareseni.com>
2025-02-28 10:42:33 +0700

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,13 +7,6 @@ return {
         ["s"] = function()
           require("flash").jump() -- Call flash.nvim's jump functionality
         end,
-        ["P"] = {
-          "toggle_preview",
-          config = {
-            use_float = false,
-            title = "Neo-tree Preview",
-          },
-        },
       },
     },
   },
