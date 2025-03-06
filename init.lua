03718a58ec6d08d70ef47e04f632110411d165b2
nurm717123 <nur@softwareseni.com>
2025-03-06 09:22:20 +0700

    add zoom in zoom out neovide

--- init.lua
+++ init.lua
@@ -8,6 +8,17 @@ if vim.g.neovide then
 
   vim.g.neovide_opacity = 0.8
   vim.g.neovide_normal_opacity = 0.8
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.15)
+  end)
 end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
