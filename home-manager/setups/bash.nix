f1ae842525df13cc036a33f0ad3f3d5894fb3ce6
runm717123 <runm717123@gmail.com>
2025-03-19 05:57:27 +0700

    force delete lock file to surpress file not exist warn

--- home-manager/setups/bash.nix
+++ home-manager/setups/bash.nix
@@ -19,7 +19,7 @@
       # bind 'set vi-ins-mode-string \1\e[5 q\2'  # Bar cursor for Insert mode
       # bind 'set vi-cmd-mode-string \1\e[1 q\2'  # Block cursor for Normal mode
 
-      rm ~/.config/nvim/lazy-lock.json
+      rm -f ~/.config/nvim/lazy-lock.json
 
       eval "$(zoxide init bash)"
     '';
