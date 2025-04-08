e22733f0114b576a6d3d63418ddf021f7814f6df
runm717123 <runm717123@gmail.com>
2025-04-08 10:47:39 +0700

    setup secret alias

--- home-manager/setups/fish.nix
+++ home-manager/setups/fish.nix
@@ -1,19 +1,25 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   programs.fish = {
     enable = true;
-    shellAliases = {
-      hs = "home-manager switch";
-      hsr = "home-manager switch && exit";
-      g = "git";
-      lg = "lazygit";
-      "..." = "cd ../..";
-      n = "nvim";
-      nn = "$HOME/dotfiles/executables/neovide.exe --wsl";
-      dev = "yarn workspace @apps/go start";
-      yw = "yarn workspace";
-      dn = "rm -f ~/.config/nvim/lazy-lock.json";
-      gpm = "git pull origin main";
-    };
+    shellAliases =
+      {
+        hs = "home-manager switch";
+        hsr = "home-manager switch && exit";
+        g = "git";
+        lg = "lazygit";
+        "..." = "cd ../..";
+        n = "nvim";
+        nn = "$HOME/dotfiles/executables/neovide.exe --wsl";
+        dev = "yarn workspace @apps/go start";
+        yw = "yarn workspace";
+        dn = "rm -f ~/.config/nvim/lazy-lock.json";
+        gpm = "git pull origin main";
+      }
+      // (lib.optionalAttrs (builtins.pathExists (config.home.homeDirectory + "/dotfiles/secret.alias.nix")) (import (config.home.homeDirectory + "/dotfiles/secret.alias.nix")));
     interactiveShellInit = ''
       rm -f ~/.config/nvim/lazy-lock.json
     '';
